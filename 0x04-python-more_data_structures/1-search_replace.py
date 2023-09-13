c0e17515ded9de3cdbaed3adc514ce77a6a7d7a2
ogbenna nmesoma anita <ogbennaanita@gmail.com>
2023-09-13 14:08:01 -0700

    updates

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    newl = my_list[:]
+    for r in range(len(new)):
+        if newl[r] == search:
+            newl[r] = replace
+    return new
