e59feda5798e87bd3fffc6f9eafa8ff5eee9db11
ogbenna nmesoma anita <ogbennaanita@gmail.com>
2023-09-13 15:05:42 -0700

    updates

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-def update_dictionary(a_dictionary, key, value):
-    if key in a_dictionary:
-        a_dictionary[key] = value
-    else:
-        a_dictionary[key] = value
-    return a_dictionary
+def multiply_list_map(my_list=[], number=0):
+    return (list(map((lambda i: i * number), my_list)))
+
