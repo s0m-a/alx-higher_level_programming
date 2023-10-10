4151b73caffc380960141238b55f0f1c968e94b3
ogbenna nmesoma anita <ogbennaanita@gmail.com>
2023-10-10 13:18:18 -0700

    updates

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+
+#!/usr/bin/python3
+"""
+This script includes the lookup function.
+"""
+
+def lookup(obj):
+"""Returns a list of an object's accessible attributes and methods."""
+return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+this is a read me fil for project Inheritance
