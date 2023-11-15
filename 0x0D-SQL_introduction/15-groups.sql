f929e2232ab3ea20a51772ec24088077f1af0917
ogbenna nmesoma anita <ogbennaanita@gmail.com>
2023-11-14 18:50:40 -0800

    updates

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
--- script that lists the number of records with the same score in the table second_table of the database
+-- script that lists the number of records with the same score in the table second_table of the DB
 
-SELECT score, COUNT(*) as number FROM second_table GROUP BY score ORDER BY numb
+SELECT score, COUNT(*) as number FROM second_table GROUP BY score ORDER BY number DESC;
